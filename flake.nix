c65cf2db7e6b90b542a9f5bc4e56964f02b0cf69
Kayla Firestack <kfirestack@mbta.com>
2024-07-30 08:56:56 -0400

    wip!wip!wip! fuck up the flake

--- flake.nix
+++ flake.nix
@@ -15,103 +15,89 @@
     utils,
     map-sprite-packer,
   }: let
-    buildRustPkgs = pkgs:
-      pkgs.callPackage ./packages/build-pbf-glyphs {};
     buildForSystem = system: let
-      overlays = [self.overlays.default];
-      pkgs = import nixpkgs {inherit system overlays;};
-      germany = {
-        name = "germany";
-        continent = "europe";
-        date = "230101";
-        sha256 = "sha256-G/9YWx4uEY6/yGVr2O5XqL6ivrlpX8Vs6xMlU2nT1DE=";
-      };
-      hessen = {
-        name = "hessen";
-        continent = "europe";
-        country = "germany";
-        date = "230101";
-        sha256 = "sha256-sOoPtIEY4TxSm/p0MGc9LGzQtQmIafGH0IWbkby95K8=";
-      };
+      pkgs = nixpkgs.legacyPackages.${system};
+      # overlays = [self.overlays.default];
 
-		massachusetts = {
-			name = "massachusetts";
-			continent = "north-america";
-			country = "us";
-			date = "240729";
-			sha256 = "sha256-CwxG44skIq1+q1GTF9P520xYalIojU/bywvT85Ye644=";
-		};
-      metadataFnFn = config: tilesUrl:
-        pkgs.buildTilesMetadata {
-          tileJson = {tiles = [tilesUrl];};
-          tiles = pkgs.buildTiles {
-            inherit config;
-            inherit (hessen) name;
-            renumber = true;
-            src = pkgs.fetchGeofabrik massachusetts;
-          };
-        };
-      tiles-demo = style:
-        (pkgs.callPackage ./demo.nix {}) {
-          bundle = pkgs.buildTilesBundle {
-            metadataFn = metadataFnFn {};
-            host = "http://127.0.0.1:8081";
-				# host = "http://macbook-air.guest.tri:8081";
-            styleFn = style;
-          };
-        };
-      tiles-nginx-bundle = style:
-        pkgs.buildTilesBundle {
-          metadataFn = metadataFnFn {settings.compress = "gzip";};
-          host = "http://127.0.0.1:8080";
-          styleFn = style;
-        };
-      vm-builder = bundle:
-        (
-          nixpkgs.lib.nixosSystem
-          {
-            system = "x86_64-linux";
-            modules = [
-              {nixpkgs.overlays = overlays;}
-              "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
-              (import ./demo-nginx.nix {inherit bundle;})
-            ];
-          }
-        )
-        .config
-        .system
-        .build
-        .vm;
+      # pkgs = import nixpkgs {inherit system overlays;};
+
+      # metadataFnFn = config: tilesUrl:
+      #   pkgs.buildTilesMetadata {
+      #     tileJson = {tiles = [tilesUrl];};
+      #     tiles = pkgs.buildTiles {
+      #       inherit config;
+      #       inherit (hessen) name;
+      #       renumber = true;
+      #       src = pkgs.fetchGeofabrik massachusetts;
+      #     };
+      #   };
+
+      # tiles-demo = style:
+      #   (pkgs.callPackage ./demo.nix {}) {
+      #     bundle = pkgs.buildTilesBundle {
+      #       metadataFn = metadataFnFn {};
+      #       host = "http://127.0.0.1:8081";
+      #       styleFn = style;
+      #     };
+      #   };
+
+      # tiles-nginx-bundle = style:
+      #   pkgs.buildTilesBundle {
+      #     metadataFn = metadataFnFn {settings.compress = "gzip";};
+      #     host = "http://127.0.0.1:8080";
+      #     styleFn = style;
+      #   };
+
+      # vm-builder = bundle:
+      #   (
+      #     nixpkgs.lib.nixosSystem
+      #     {
+      #       system = "x86_64-linux";
+      #       modules = [
+      #         {nixpkgs.overlays = overlays;}
+      #         "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
+      #         (import ./demo-nginx.nix {inherit bundle;})
+      #       ];
+      #     }
+      #   )
+      #   .config
+      #   .system
+      #   .build
+      #   .vm;
     in {
-      packages = {
-        inherit (pkgs) build_pbf_glyphs;
-      };
       legacyPackages = {
-        # styles = lib.mapAttrs () pkgs.tilesStyles
+        scope = pkgs.callPackage ./scope.nix {
+          makeScope = pkgs.lib.makeScope;
+        };
+
         inherit (pkgs) tilesStyles;
       };
-      apps = builtins.foldl' (left: right: left // right) {} (map (
-          key: {
-            "demo-${key}" = {
-              type = "app";
-              program = "${tiles-demo pkgs.tilesStyles.${key}}/bin/demo";
-            };
-            "demo-nginx-${key}" = {
-              type = "app";
-              program = "${vm-builder (tiles-nginx-bundle pkgs.tilesStyles.${key})}/bin/run-nixos-vm";
-            };
-          }
-        ) [
-          "maptiler-basic-gl-style"
-          "osm-bright-gl-style"
-          "positron-gl-style"
-          "dark-matter-gl-style"
-          "maptiler-terrain-gl-style"
-          "maptiler-3d-gl-style"
-          "fiord-color-gl-style"
-          "maptiler-toner-gl-style"
-          "osm-liberty"
-        ]);
+
+      # packages = {
+      #   inherit (pkgs) build_pbf_glyphs;
+      # };
+      # apps = builtins.foldl' (left: right: left // right) {} (map (
+      #     key: {
+      #       "demo-${key}" = {
+      #         type = "app";
+      #         program = "${tiles-demo pkgs.tilesStyles.${key}}/bin/demo";
+      #       };
+      #       "demo-nginx-${key}" = {
+      #         type = "app";
+      #         program = "${vm-builder (tiles-nginx-bundle pkgs.tilesStyles.${key})}/bin/run-nixos-vm";
+      #       };
+      #     }
+      #   ) [
+      #     "maptiler-basic-gl-style"
+      #     "osm-bright-gl-style"
+      #     "positron-gl-style"
+      #     "dark-matter-gl-style"
+      #     "maptiler-terrain-gl-style"
+      #     "maptiler-3d-gl-style"
+      #     "fiord-color-gl-style"
+      #     "maptiler-toner-gl-style"
+      #     "osm-liberty"
+      #   ]);
     };
   in
     (utils.lib.eachDefaultSystem buildForSystem)
@@ -119,18 +105,18 @@
       overlays = {
         default = nixpkgs.lib.composeManyExtensions [
           map-sprite-packer.overlays.default
-          self.overlays.packages
+          # self.overlays.packages
         ];
-        packages = final: prev: {
-          build_pbf_glyphs = buildRustPkgs prev;
-          fetchGeofabrik = prev.callPackage ./fetch-geofabrik.nix {};
-          buildTiles = prev.callPackage ./build-tiles.nix {};
-          buildTilesBundle = prev.callPackage ./build-bundle.nix {};
-          buildTilesFonts = prev.callPackage ./build-fonts.nix {};
-          buildTilesStyle = prev.callPackage ./build-style.nix {};
-          buildTilesMetadata = prev.callPackage ./build-metadata.nix {};
-          tilesStyles = final.callPackage ./styles.nix {};
-        };
+        # packages = final: prev: {
+        #   build_pbf_glyphs = buildRustPkgs prev;
+        #   fetchGeofabrik = prev.callPackage ./fetch-geofabrik.nix {};
+        #   buildTiles = prev.callPackage ./build-tiles.nix {};
+        #   buildTilesBundle = prev.callPackage ./build-bundle.nix {};
+        #   buildTilesFonts = prev.callPackage ./build-fonts.nix {};
+        #   buildTilesStyle = prev.callPackage ./build-style.nix {};
+        #   buildTilesMetadata = prev.callPackage ./build-metadata.nix {};
+        #   tilesStyles = final.callPackage ./styles.nix {};
+        # };
       };
     };
 }
