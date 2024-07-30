e68a392ef1f417b2a01f613b108d004433140eb9
Kayla Firestack <kfirestack@mbta.com>
2024-07-30 08:56:56 -0400

    wip! feat: tilemaker-shp-files-runcommand

--- scope.nix
+++ scope.nix
@@ -38,5 +38,37 @@ makeScope newScope (self: {
 		sha256 = "sha256-qy2uQnHf8leLPaf3tvu8Pp5UiOapSaUfBXtcu8Kgz4o=";
 	};
 
+	tilemaker-shp-files-runcommand = self.callPackage (
+		{ lib
+		, runCommand
+		, fetchFromGitHub
+		, curl
+		, cacert
+		, unzip
+		}: runCommand "tilemaker-shp-files" {
+
+			src = fetchFromGitHub {
+				owner = "systemed";
+				repo = "tilemaker";
+				rev = "eab08d189ad97ddf5db7d915bcabe42ad3dab6af";
+				hash = "sha256-A4I2xwB7E+7iwaLt8NGoAVrPLSmu6l8wNURu9EUqyTk=";
+			};
+
+			outputHashAlgo = "sha256";
+			outputHashMode = "recursive";
+			outputHash = "sha256-SLMz2tAEj78/irgKzORuaOrSYaMpOZII9HPQlvZrzUk=";
+
+			buildInputs = [ curl unzip ];
+
+			SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt";
+		}
+		(lib.concatLines [
+			"mkdir -p $out"
+			"pushd $out"
+			"bash -x $src/get-landcover.sh"
+			"bash -x $src/get-coastline.sh"
+		])
+	) {};
+
 	buildDemo = self.callPackage ./demo.nix {};
 })
