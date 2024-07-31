09816fec810f873ab48701e370b030fc71cfd1cf
Kayla Firestack <kfirestack@mbta.com>
2024-07-30 22:48:02 -0400

    wip! tileserver-gl npm

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
 { pkgs ? import <nixpkgs> {}
 , mkShellNoCC ? pkgs.mkShellNoCC
 , nodejs_20 ? pkgs.nodejs_20
+, pkg-config ? pkgs.pkg-config
+, pixman ? pkgs.pixman
+, cairo ? pkgs.cairo
+, pango ? pkgs.pango
 }: mkShellNoCC {
 	packages = [
 		nodejs_20
+		pkg-config
+		pixman
+		cairo
+		pango
 	];
 }
