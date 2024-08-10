8dbb0d89d3d946d7a942645d7e0581677f4731c9
Kayla Firestack <kfirestack@mbta.com>
2024-08-10 00:11:40 -0400

    wip! refactor! build tiles

--- build-tiles.nix
+++ build-tiles.nix
@@ -8,64 +8,56 @@
 , unzip
 , osmium-tool
 , tilemaker-shp-files
-}: {
-  name,
-  src,
-  config ? {},
-  renumber ? false,
-}: let
-  tilemaker-config =
-    writeText "app-config.json" (builtins.toJSON
-      ({settings.compress = "none";} // config));
-  args =
-    if renumber
-    then "--compact"
-    else "";
-  isCompressed =
-    (builtins.hasAttr "settings" config)
-    && (builtins.hasAttr "compress" config.settings)
-    && config.settings.compress != "none";
-  # then ''find tiles -name '*.pbf' -exec sh -c 'echo "$1" "$1.gz"' - '{}' +''
-in
-  stdenv.mkDerivation rec {
+}:
+{ name
+, extension ? "pmtiles"
+, src
+, config ? {}
+, renumber ? false
+}: stdenv.mkDerivation rec {
     inherit src;
-    name = "${src.name or ""}.mbtiles";
+    name = "${src.name or ""}.${extension}";
 
-    unpackPhase = "true";
+    # unpackPhase = "true";
 
-    buildInputs = [jq mbutil tilemaker unzip osmium-tool];
+    buildInputs = [jq mbutil tilemaker unzip];
+
+    passthru.tilemaker-config =
+      writeText "app-config.json" (
+        builtins.toJSON ({settings.compress = "none";} // config)
+      );
 
     passthru.config = runCommand "tilemaker-config.json" {
       buildInputs = [ jq ];
     } ''
       jq -c '. * input' \
         ${tilemaker}/share/tilemaker/config-openmaptiles.json \
-        ${tilemaker-config} \
+        ${passthru.tilemaker-config} \
         > $out
     '';
 
-    # passthru.data =
-    #   if renumber
-    #   then runCommand "data.osm.pbf" {} ''
-    #     osmium renumber -i. -o $out ${src}
-    #   ''
-    #   else src;
+    dontUnpack = true;
+    passthru.data =
+      if renumber
+      then runCommand "${src.name or ""}.renumbered.osm.pbf" {
+        buildInputs = [osmium-tool];
+      } "osmium renumber -i. -o $out ${src}"
+      else src;
 
     buildPhase = ''
       ln -s ${tilemaker-shp-files}/landcover .
       ln -s ${tilemaker-shp-files}/coastline .
       ls -la
-      ${
-        if renumber
-        then "osmium renumber -i. -o data.osm.pbf ${src}"
-        else "ln -s ${src} data.osm.pbf"
-      }
 
       tilemaker \
-        --input data.osm.pbf ${args} \
+        --input ${passthru.data} \
         --output $out \
         --config=${passthru.config} \
-        --process=${tilemaker}/share/tilemaker/process-openmaptiles.lua
+        --process=${tilemaker}/share/tilemaker/process-openmaptiles.lua \
+        ${if renumber
+          then "--compact"
+          else ""
+        }
     '';
 
     dontInstall = true;
